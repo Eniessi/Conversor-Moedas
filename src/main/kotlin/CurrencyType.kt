e28ced0dae3b7439cac388883aee102a87235a41
Nathan Silva de Souza <nathan_bzss@hotmail.com>
2025-11-29 18:55:29 -0300

    Feat: Configuração das rotas que serão usadas

---
+++ src/main/kotlin/CurrencyType.kt
@@ -0,0 +1,80 @@
+package com
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CurrencyType(
+    val name : String,
+    val acronym: String,
+    val symbol : String,
+    val countryFlagImageUrl : String
+)
+
+@Serializable
+data class CurrencyTypeResult(
+    val values: List<CurrencyType>
+)
+
+// Lista estática simulando um Banco de Dados ou resposta de API
+val currencyTypes = listOf(
+    CurrencyType(
+        name = "Dólar Americano",
+        acronym = "USD",
+        symbol = "$",
+        countryFlagImageUrl = "https://flagcdn.com/w320/us.png"
+    ),
+    CurrencyType(
+        name = "Euro",
+        acronym = "EUR",
+        symbol = "€",
+        countryFlagImageUrl = "https://flagcdn.com/w320/eu.png"
+    ),
+    CurrencyType(
+        name = "Real Brasileiro",
+        acronym = "BRL",
+        symbol = "R$",
+        countryFlagImageUrl = "https://flagcdn.com/w320/br.png"
+    ),
+    CurrencyType(
+        name = "Libra Esterlina",
+        acronym = "GBP",
+        symbol = "£",
+        countryFlagImageUrl = "https://flagcdn.com/w320/gb.png"
+    ),
+    CurrencyType(
+        name = "Iene Japonês",
+        acronym = "JPY",
+        symbol = "¥",
+        countryFlagImageUrl = "https://flagcdn.com/w320/jp.png"
+    ),
+    CurrencyType(
+        name = "Dólar Canadense",
+        acronym = "CAD",
+        symbol = "C$",
+        countryFlagImageUrl = "https://flagcdn.com/w320/ca.png"
+    ),
+    CurrencyType(
+        name = "Dólar Australiano",
+        acronym = "AUD",
+        symbol = "A$",
+        countryFlagImageUrl = "https://flagcdn.com/w320/au.png"
+    ),
+    CurrencyType(
+        name = "Yuan Chinês",
+        acronym = "CNY",
+        symbol = "¥",
+        countryFlagImageUrl = "https://flagcdn.com/w320/cn.png"
+    ),
+    CurrencyType(
+        name = "Franco Suíço",
+        acronym = "CHF",
+        symbol = "Fr",
+        countryFlagImageUrl = "https://flagcdn.com/w320/ch.png"
+    ),
+    CurrencyType(
+        name = "Peso Argentino",
+        acronym = "ARS",
+        symbol = "$",
+        countryFlagImageUrl = "https://flagcdn.com/w320/ar.png"
+    )
+)
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,5 +1,7 @@
 package com
 
+import com.currencyTypes
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
@@ -8,8 +10,24 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        // tipos de moedas (nome, acronimo, símbolo, imagem bandeira do país)
+        get("/currency_types") {
+            call.respond(CurrencyTypeResult(
+                values = currencyTypes
+            ))
+        }
+
+        //conversão de valores, moeda atual => moeda alvo, taxa de conversoa entre as moedas.
+        //from e to serão os acronimos de cada moeda
+        get("/exchange_rate/{from}/{to}") {
+            val from = call.parameters["from"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Erro ao obter acrônimo da moeda atual."
+            )
+            val to = call.parameters["to"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Erro ao obter acrônimo da moeda alvo."
+            )
         }
     }
 }
